e9566a64b6f811e1d0e9cfb2592a2efb019f47ac
Dora <domonshieh@gmail.com>
2021-01-12 16:53:53 +0800

    Initial Commit

---
+++ STANDBYME2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  STANDBYME2
+//
+//  Created by 謝孟芩 on 2021/1/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
